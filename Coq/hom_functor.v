c13bee61ee01aca58693a8349113e810fc3a47b0
Nathan Brader <nbrader@gmail.com>
2024-05-02 13:36:20 +0100

    Split Category class into it's own file

--- Coq/hom_functor.v
+++ Coq/hom_functor.v
@@ -1,18 +1,6 @@
 Require Import Coq.Arith.Arith.
 Require Import Coq.Logic.ProofIrrelevance.
-
-Class Category := {
-  Obj : Type;  (* Type of objects in the category *)
-  Mor : Obj -> Obj -> Type;  (* Type of morphisms between objects *)
-  
-  id : forall {X}, Mor X X;  (* Identity morphism *)
-  compose : forall {X Y Z}, Mor Y Z -> Mor X Y -> Mor X Z;  (* Composition of morphisms *)
-
-  left_identity : forall {X Y} (f : Mor X Y), compose id f = f;
-  right_identity : forall {X Y} (f : Mor X Y), compose f id = f;
-  associativity : forall {X Y Z W} (f : Mor W X) (g : Mor X Y) (h : Mor Y Z),
-                  compose h (compose g f) = compose (compose h g) f
-}.
+Require Import HomFunctor.StructCategory.
 
 (* Example of a finite category based on the integers 0-3 *)
 Inductive FiniteInt : Type :=
